f52431a5913891babf2aaaf2f198ba7e4e7ab278
Ivan <isandiyu@gmail.com>
2024-05-07 16:01:37 -0300

    agregue contacto y titulos

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ import postgres from './imagenes/postgres.png';
 import html from './imagenes/html.png';
 import css from './imagenes/css.png';
 import htmlJsCss from './imagenes/html-css-js.png';
+import asp from './imagenes/asp.png';
+import linkedin from './imagenes/linkedin.svg';
+import git from './imagenes/git.png';
+import gmail from './imagenes/gmail.svg';
+import maletin from './imagenes/maletin.svg';
 
 
 const AbrirGit = () => {
@@ -31,9 +36,9 @@ function App() {
           <h1>Desarrollador full stack</h1>
           <nav>
             <a href="#">Inicio</a>
-            <a href="#">Conocimientos</a>
-            <a href="#">Proyectos</a>
-            <a href="#">Contacto</a>
+            <a href="#conocimientos">Conocimientos</a>
+            <a href="#proyecto-inicio">Proyectos</a>
+            <a href="#contact">Contacto</a>
           </nav>
         </div>
 
@@ -46,53 +51,73 @@ function App() {
             <img src={img} alt="Iván Sandiyú" id="imagen"></img>
           </div>
         </div>
-        <div className="conocimientos">
+
+        <p className='texto-conocimiento' id='conocimientos'>Conocimientos</p>
+        <div className="conocimientos" >
           <div class="columna">
             <img src={csharp} alt="Imagen 1" id="imagen" />
             <p></p>
           </div>
           <div class="columna">
-            <img src={java} alt="Imagen 2" id="imagen"/>
+            <img src={java} alt="Imagen 2" id="imagen" />
             <p></p>
           </div>
           <div class="columna">
-            <img src={js} alt="Imagen 3" id="imagen"/>
+            <img src={js} alt="Imagen 3" id="imagen" />
             <p></p>
           </div>
           <div class="columna">
-            <img src={react} alt="Imagen 4" id="imagen"/>
+            <img src={react} alt="Imagen 4" id="imagen" />
             <p></p>
           </div>
           <div class="columna">
-            <img src={sql} alt="Imagen 5" id="imagen"/>
+            <img src={sql} alt="Imagen 5" id="imagen" />
             <p></p>
           </div>
         </div>
 
-        <div className="proyectos">
+
+        <p className='texto-proyecto' id='proyecto-inicio'>Proyectos</p>
+        <div className="proyectos" >
           <div className="contenedor-proyecto">
             <div className="imagen-proyecto">
-              <img src={htmlJsCss} alt="Imagen del proyecto" id="htmlCssJs"/>
+              <img src={htmlJsCss} alt="Imagen del proyecto" id="htmlCssJs" />
             </div>
             <div className="contenido">
               <h1>Portafolio primer version</h1>
-              <p >Primer portafolio realizado</p>
+              <p >Primer portafolio realizado con HTML-CSS-JAVASCRIPT</p>
               {/* <button onClick={AbrirGit()}></button> */}
             </div>
           </div>
         </div>
 
         <div className="proyectos">
-        <div className="contenedor-proyecto">
+
+          <div className="contenedor-proyecto">
             <div className="imagen-proyecto-crud">
-              <img src={python} alt="Imagen del proyecto"  />
-              <img src={postgres} alt="Imagen del proyecto"  />
+              <img src={python} alt="Imagen del proyecto" />
+              <img src={postgres} alt="Imagen del proyecto" />
               <img src={react} alt="Imagen del proyecto" />
             </div>
             <div className="contenido-crud">
               <h1>Sistema CRUD</h1>
-              <p>Primer portafolio realizado</p>
-              
+              <p>FastAPI Python-React-PostgreSQL</p>
+
+            </div>
+          </div>
+        </div>
+
+        <div className="proyectos">
+          <div className="contenedor-proyecto">
+            <div className="imagen-proyecto-asp">
+              <img src={sqlServer} alt="Imagen del proyecto" />
+              <img src={asp} alt="Imagen del proyecto" />
+              <img src={csharp} alt="Imagen del proyecto" />
+            </div>
+            <div className="contenido">
+              <h1>Sistema CRUD</h1>
+              <p>Crud realizado con C#-ASP-MVC-SQL Server</p>
+
             </div>
           </div>
         </div>
@@ -104,8 +129,8 @@ function App() {
             </div>
             <div className="contenido">
               <h1>CRUD local con Java</h1>
-              <p>Primer portafolio realizado</p>
-             
+              <p>Crud basico con java</p>
+
             </div>
           </div>
         </div>
@@ -117,8 +142,8 @@ function App() {
             </div>
             <div className="contenido">
               <h1>CRUD local con C#</h1>
-              <p>Primer portafolio realizado</p>
-              
+              <p>Sistema crud</p>
+
             </div>
           </div>
         </div>
@@ -130,10 +155,26 @@ function App() {
             </div>
             <div className="contenido">
               <h1>Alta y baja de mesa</h1>
-              <p>Primer portafolio realizado</p>
+              <p>Altas y bajas de mesas de examenes</p>
             </div>
           </div>
         </div>
+
+        <div className='contenedor-contacto' id='contact'>
+          <div className='contacto'>
+            <h3>Hola! Mi nombre es Iván y estoy a punto de recibirme como Tecnico Universitario en programacion!. Estoy en busca de mi primer experiencia laboral para poder completar mi formación académica</h3>
+            <p>Contacto</p>
+          </div>
+          <div className='imagenes-contacto'>
+            <img src={git} alt="Imagen del proyecto" />
+            <img src={linkedin} alt="Imagen del proyecto" />
+            <img src={gmail} alt="Imagen del proyecto" />
+
+
+          </div>
+
+        </div>
+
 
 
 
